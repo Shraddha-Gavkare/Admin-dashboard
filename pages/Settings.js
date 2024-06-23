3c53a57877275b2153fc6a6085db594b2cf14937
Shraddha-Gavkare <112618249+Shraddha-Gavkare@users.noreply.github.com>
2024-06-23 11:26:11 -0600

    Add files via upload

---
+++ pages/Settings.js
@@ -0,0 +1,15 @@
+// src/pages/Settings.js
+
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Settings = () => {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>Settings</Typography>
+      {/* Add content here */}
+    </div>
+  );
+};
+
+export default Settings;
